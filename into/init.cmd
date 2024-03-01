a2243b2d602f895aaa7d1092f09f1b634a62bacf
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-01 09:19:00 -0300

    run

---
+++ into/init.cmd
@@ -0,0 +1,3 @@
+@echo off
+python into.py 
+cmd
---
+++ into/into.py
@@ -0,0 +1,18 @@
+title = ''' Hello!!
+'''
+print(title)
+p = '''
+DearESL/EFL Collegues,
+
+    I once met a teacher who had recently used one of my texts in her class.
+'''
+t = '''
+Queridos ESL/EFL colegas,
+
+    Certa vez conheci um professor que tinha recentemente usado um dos meus textos em suas aulas.
+'''
+
+print(p)
+try_ = input("Tente Traduzir, o que você acha que siginifica?\n")
+
+print(t)
