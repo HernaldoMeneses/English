6eb8b1445f696068dd7bb3509ee5b32196a17dcb
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-25 17:14:15 -0300

    Update into.py

--- into/into.py
+++ into/into.py
@@ -2,25 +2,32 @@
 
 text_all = []
 
+def groop_to (read_Eng, read_Port):
+    read_Eng = read_Eng
+    read_Port = read_Port
+    read_Key = {read_Eng:read_Port}
+    text_all.append(read_Key)
+
 read_Eng = "DearESL/EFL Collegues, I once met a teacher who had recently used one of my texts in her class."
 read_Port = ""
-read_Key = {read_Eng:read_Port}
-text_all.append(read_Key)
+groop_to (read_Eng, read_Port)
+
 
 read_Eng = "At the end of the rem, one of her students said to her."
 read_Port = ""
-read_Key = {read_Eng:read_Port}
-text_all.append(read_Key)
-
-read_Eng = "At the end of the rem, one of her students said to her."
-read_Port = ""
-read_Key = {read_Eng:read_Port}
-text_all.append(read_Key)
+groop_to (read_Eng, read_Port)
 
 read_Eng = "'Thank you for teaching me the secrets of English.'"
 read_Port = ""
-read_Key = {read_Eng:read_Port}
-text_all.append(read_Key)
+groop_to (read_Eng, read_Port)
+
+read_Eng = "I still smile when I think of that comment."
+read_Port = ""
+groop_to (read_Eng, read_Port)
+
+read_Eng = ""
+read_Port = ""
+groop_to (read_Eng, read_Port)
 
 
 
